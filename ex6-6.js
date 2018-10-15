9d3fb4deb1af662bb90f6a8436c19f1b6af16bbf
zm16537 <zm16537@josai.ac.jp>
2018-10-14 21:59:00 -0400

    message

---
+++ ex6-6.js
@@ -0,0 +1,4 @@
+var who66='world';
+function sayhello66(){alert('Hello,'+who66+'!');}
+function taro66(){who66='太郎';}
+function hanako66(){who66='花子';}
---
+++ ex6-7.js
@@ -0,0 +1,3 @@
+var who='world';
+function sayhello67(){alert('Hello,'+who+'!')}
+function someone(x){who=x;}
