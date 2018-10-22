f79dd04c941a7a1c05b2e6dd5b6a28182f0bc2e2
zm16537 <zm16537@josai.ac.jp>
2018-10-21 21:54:42 -0400

    20181022

--- ex6-8.js
+++ ex6-8.js
@@ -1,4 +1,4 @@
 function sayhello68(){
-  alert('Hello,'+document.getElementByld('who68').innerHTML+'!');}
-function taro68(){document.getElementByld('who68').innerHTML='太郎';}
-function hanako68(){document.getElementByld('who68').innerHTML='花子';}
+  alert('Hello,'+document.getElementById('who68').innerHTML+'!');}
+function taro68(){document.getElementById('who68').innerHTML='太郎';}
+function hanako68(){document.getElementById('who68').innerHTML='花子';}
