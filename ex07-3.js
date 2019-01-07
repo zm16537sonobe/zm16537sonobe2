44ba0c5dcfae31946322fb9cdfd50492fe91d074
zm16537 <zm16537@josai.ac.jp>
2019-01-06 20:51:27 -0500

    20190107

---
+++ ex07-3.js
@@ -0,0 +1,9 @@
+function add(){
+var x=document.getElementById('input1').value;
+var y=document.getElementById('input2').value;
+x=Number(x)
+y=Number(y)
+  
+  alert(x+y);
+  
+}
---
+++ ex07-4.js
@@ -0,0 +1,7 @@
+function add1(){
+ var x=document.getElementById('input3').value;
+ var y=document.getElementById('input4').value;
+  x=Number(x)
+  y=Number(y)
+  alert(Number('x')/Number('y*y'));
+}
